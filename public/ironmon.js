f32c1d6069fd369bb07f10e095643dffe703a48e
Carlos Jones <carlosjones@Carloss-MBP.home>
2015-06-16 04:06:57 -0400

    ironmon commit

--- public/ironmon.js
+++ public/ironmon.js
@@ -9,7 +9,9 @@
     - name
 */
 var Ironmon = function(name) {
-
+    this.health = 25
+    this.power = 1
+    this.name = name
 }
 
 /*
@@ -21,14 +23,15 @@ var Ironmon = function(name) {
     make the Ironmon's health go above 25, it becomes 25 instead.
 */
 Ironmon.prototype.heal = function() {
-
+var heal = this.health += Math.floor(Math.random() * 2) + 1
+if(heal===2){return 25}
 }
 
 /*
   This function adds one to the power of the Ironmon.
 */
 Ironmon.prototype.train = function() {
-
+var train = this.power +=1
 }
 
 /*
@@ -37,9 +40,8 @@ Ironmon.prototype.train = function() {
   Otherwise, it returns false.
 */
 Ironmon.prototype.active = function() {
-
-}
-
+(this.health >=1)
+} 
 /*
   This function accepts another, opponent Ironmon as a parameter.
 
@@ -49,5 +51,6 @@ Ironmon.prototype.active = function() {
   This function returns the amount of damage dealt.
 */
 Ironmon.prototype.attack = function(opponent) {
-  
+  var attack = Ironmon.health -= Math.floor(Math.random() * Ironmon.power) + 1
+  return attack
 }
